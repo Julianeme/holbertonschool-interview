13927ff049d92b58257d9adcf054aef293a1ee63
Julianeme <juliantve@gmail.com>
2021-12-09 21:08:17 -0500

    Code changed to verify if key is any other box but in its own box

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,1 +1,37 @@
-#bin/bash/python 3
+
+#usr/bin/env python3
+# algoritmh to solve the lockboxes problem
+
+
+def canUnlockAll(boxes):
+    """
+    Return True if all boxes can be opened, False otherwise.
+    """
+    keys_found = {}
+    answer = True
+    i = 0
+    while (i < len(boxes)):
+        keys_found[i] = boxes[i]
+        i += 1
+
+    for box_key in range (1, len(boxes)):
+        for i in range (0, len(boxes)):
+            if (box_key in keys_found[i] and box_key != i):
+                break
+            elif (box_key not in keys_found[i] and i == len(boxes) - 1):
+                answer = False
+                return answer
+    return answer
+
+
+
+
+
+    #     if (len(keys_found[i]) > 0):
+    #         print("entro")
+    #         for j in range (len(boxes[i])):
+    #             key = boxes[i][j]
+    #             keys_found[key] = boxes[i][j]
+    #         print(keys_found[1])
+    # print (keys_found)
+
