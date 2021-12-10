e786e9bdf2b304d916dc0aebfbbeeb3fd3d5e563
Julianeme <juliantve@gmail.com>
2021-12-09 21:11:44 -0500

    pep8 style correction

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,6 +1,5 @@
-
-#usr/bin/env python3
-# algoritmh to solve the lockboxes problem
+#!/usr/bin/python3
+""" algorithm to solve the lockboxes problem"""
 
 
 def canUnlockAll(boxes):
@@ -14,24 +13,11 @@ def canUnlockAll(boxes):
         keys_found[i] = boxes[i]
         i += 1
 
-    for box_key in range (1, len(boxes)):
-        for i in range (0, len(boxes)):
+    for box_key in range(1, len(boxes)):
+        for i in range(0, len(boxes)):
             if (box_key in keys_found[i] and box_key != i):
                 break
             elif (box_key not in keys_found[i] and i == len(boxes) - 1):
                 answer = False
-                return answer
+                return (answer)
     return answer
-
-
-
-
-
-    #     if (len(keys_found[i]) > 0):
-    #         print("entro")
-    #         for j in range (len(boxes[i])):
-    #             key = boxes[i][j]
-    #             keys_found[key] = boxes[i][j]
-    #         print(keys_found[1])
-    # print (keys_found)
-
